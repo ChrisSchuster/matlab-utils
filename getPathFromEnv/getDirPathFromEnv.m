f67b460b3e1e09e022d836ec5ac8d0d3596f8a4f
Christopher Schuster <christopher.schuster@reiter-engineering.com>
2023-01-13 22:18:39 +0100

    - implemented creating the environmental variable when it is not found   - in that case, two folder selection dialogues are presented   - first, select the default directory   - second, select the desired source directy for the invocing script - documentation

--- getPathFromEnv/getDirPathFromEnv.m
+++ getPathFromEnv/getDirPathFromEnv.m
@@ -1,19 +1,25 @@
 function [fsPath] = getDirPathFromEnv()
-%GETPATHFROMENV Returns a path from .env file at the root of the invocing script
+%GETDIRPATHFROMENV Returns a path from environment variable at the root of the invocing script
 %   input: empty
 %   output: filesystem path as char
 %   
-%   Usage examples: fsPath = uigetdir(getPathFromEnv());
+%   Usage examples: fsPath = getPathFromEnv();
 %   
-%   ToDo: handle missing .env file -> creating it
 
-envExists = isfile('.env');
+envExists = isfile('.env');                                 % check if environmental variable is set
 
-if envExists
-    fID = fopen(fullfile(cd,'.env'),'r');
-    fsPath = fscanf(fID,'%c');
-    status = fclose(fID);
+if envExists                                                % read path from environment varibale if set
+    fID = fopen(fullfile(cd,'.env'),'r');                   % open the file containing the env variable
+    envPath = fscanf(fID,'%c');                             % parse the file contents, returns path stored in the env variable
+else                                                        % create the env variable if not found, store the desired default path
+    envPath = uigetdir("C:\",'Define default directory');   % open folder selection dialog box
+    fID = fopen('.env','w');                                % create the environmental variable in the root of the invocing script
+    fprintf(fID,'%s',envPath);                              % write the desired default path to the env variable
 end
+
+fsPath = uigetdir(envPath,'Select source directory');       % open the folder selection dialog box
+
+status = fclose(fID);                                       % close the file containing the environment varibale
 
 end
 
