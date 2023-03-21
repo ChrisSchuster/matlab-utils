00b3b698b2c4e0faa9ce5fa048b9de8ff96fa062
Christopher Schuster <christopher.schuster@reiter-engineering.com>
2023-03-21 11:12:48 +0100

    created function

---
+++ derivate.m
@@ -0,0 +1,9 @@
+function [d_signal] = derivate(signal, timestep, varargin)
+%DERIVATE calculate the derivative of a signal with respect to time
+%   signal:     vector of the signal
+%   timestep:   
+
+
+
+end
+
