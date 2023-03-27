e227701f8d01eca612b9da6933f97130e5049020
Christopher Schuster <christopher.schuster@reiter-engineering.com>
2023-03-27 11:43:57 +0200

    change how detection intervals are passed to the function
    - before you would pass the min and max value as separate parameters
    - now you pass either a scalar or vector value for the interval
      - if it is a scalar
        - if there is a width argument use the width value to create an interval
        - if there is no width argument, the detection window will be centered around zero with the value as upper and lower bounds
      - if it is a vector:
        - the first value is the lower limit
        - the second value is the upper limit

--- isBetween.m
+++ isBetween.m
@@ -1,11 +1,30 @@
-function [state] = isBetween(signal,min,max)
+function [state] = isBetween(signal, interval, width)
 %ISBETWEEN returns true when signal is between min and max
-%   Detailed explanation goes here
+%   interval:   interval of interest
+%   width:      width of the detection band in case of scalar interval
+%
+%   interval:
+%       vector:             [lower upper]
+%       scalar w width:     [center] (+/- width)
+%       scalar w/o width:   [-value value]
+
+if isscalar(interval)
+    if nargin < 3
+        min = -1 * interval;
+        max =      interval;
+    else
+        min = interval - width;
+        max = interval + width;
+    end
+else
+    min = interval(1);
+    max = interval(2);
+end
 
 state1 = signal > min;
 state2 = signal < max;
 
-state = and(state1,state2);
+state = and(state1, state2);
 
 end
 
