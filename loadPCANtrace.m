993539cb0ab361cae46e7d93853b3da0c5512a24
Christopher Schuster <christopher.schuster@reiter-engineering.com>
2023-11-20 17:36:35 +0100

    parsing the CAN trace is now done in the main function file
    - the functionality will most likely not be used outside of this feature
    - add script to explain, how the formula was found to calcucalte the start bit for big endian signals

--- loadPCANtrace.m
+++ loadPCANtrace.m
@@ -3,19 +3,57 @@ function trace = loadPCANtrace(dbcPath, tracePath)
 %   dbcPath:    path to the DBC file
 %   tracePath:  path to the CAN trace file
 
-dbc = canDatabase(dbcPath);     % load the dbc
-rawTrace = traceRead(tracePath);
+dbc = canDatabase(dbcPath);                                                 % load the dbc
+
+%% load the trace
+firstDataLine = 17;                                                         % ignore the header of the trace file
+numVars = 13;                                                               % define the number of columns the trace file is structured into
+
+opts = delimitedTextImportOptions('NumVariables', numVars);
+opts.VariableNames = {'var1',   'timeOffs', 'direction',   'ID',     'DLC',    'byte0',  'byte1',  'byte2',  'byte3',  'byte4',  'byte5',  'byte6',  'byte7'};
+opts.VariableTypes = {'single', 'double',   'categorical', 'string', 'single', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string'};
+opts.SelectedVariableNames = 2:numVars;
+opts.DataLines = firstDataLine;
+opts.Delimiter = " ";
+opts.ConsecutiveDelimitersRule = "join";
+opts.LeadingDelimitersRule = "ignore";
+opts.TrailingDelimitersRule = "ignore";
+
+opts = setvaropts(opts, 4, 'NumberSystem', 'hex', 'FillValue', NaN, 'Type', 'uint16');  % special read instructions for the CAN ID column
+
+for variable = 6:13
+    opts = setvaropts(opts, variable, 'NumberSystem', 'hex', 'FillValue', NaN, 'Type', 'uint64');
+end
+
+
+msgtable = readtable(tracePath, opts);
+
+msgtable.timeOffs = duration(0,0,0,msgtable.timeOffs);
+msgtable.timeOffs.Format = "s";
+
+bits = 8;
+
+msgtable.data = bitshift(msgtable.byte0, 0*bits) + ...
+                bitshift(msgtable.byte1, 1*bits) + ...
+                bitshift(msgtable.byte2, 2*bits) + ...
+                bitshift(msgtable.byte3, 3*bits) + ...
+                bitshift(msgtable.byte4, 4*bits) + ...
+                bitshift(msgtable.byte5, 5*bits) + ...
+                bitshift(msgtable.byte6, 6*bits) + ...
+                bitshift(msgtable.byte7, 7*bits);
+
+%% parse the CAN trace message table
 
 trace = struct();
 nMsg = numel(dbc.Messages);
 
 for msgIDidx = 1:nMsg       % message ID index
 
-    mask = rawTrace.ID == dbc.MessageInfo(msgIDidx).ID;
+    mask = msgtable.ID == dbc.MessageInfo(msgIDidx).ID;
     msgName = dbc.Messages{msgIDidx};
-    data = rawTrace(mask,:).data;
+    data = msgtable(mask,:).data;
 
-    trace.(msgName) = rawTrace(mask,1);
+    trace.(msgName) = msgtable(mask,1);
 
     nSignals = numel(dbc.MessageInfo(msgIDidx).Signals);
     for sig = 1:nSignals
